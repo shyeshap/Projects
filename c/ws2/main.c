a7debf448cbd46e09074f2e8bfd8f59f4162d6a9
shye, shapira <shyesh@gmail.com>
2019-11-04 13:04:12 +0200

    update 4/11/19 13:00

--- c/ws2/main.c
+++ c/ws2/main.c
@@ -5,23 +5,27 @@
 #include <assert.h>
 #include <string.h>
 #include <stdlib.h>
+#include <mystring.h>
 
 size_t mystrLen(const char *s);
 int MyStrCmp(const char *s1, const char *s2);
 char *MyStrCpy(char *dest, const char *src);
 char *strncpy(char *dest, const char *src, size_t n);
+int mystrcasecmp(const char *s1, const char *s2);
+char *strchr(const char *s, int c);
+
 
 int main()
 {
 	char s[] = "Hello world";
 	printf("the length of %s is %ld\n\n", s,mystrLen(s));
 	
-
-	/*test*/
 	char s1[] = "ac";
 	char s2[] = "A";
 	char s3[] = "";
 	char s4[] = "achg";
+	
+	/*test strcmp*/
 	printf("%d\n", strcmp(s1, s4));
 	printf("%d\n", strcmp(s1, s2));
 	printf("%d\n\n", strcmp(s2, s1));
@@ -30,14 +34,43 @@ int main()
 	printf("%d\n", MyStrCmp(s1, s2));
 	printf("%d\n\n", MyStrCmp(s2, s1));
 	
+	/*test strcpy*/
 	printf("%s\n", strcpy(s1, s2));
 	printf("%s\n", strcpy(s1, s3));
 	printf("%s\n\n", strcpy(s1, s4));
 
-	printf("%s\n", MyStrCpy(s1, s2));
-	printf("%s\n", MyStrCpy(s1, s3));
-	printf("%s\n", MyStrCpy(s1, s4));
+	char b1[] = "ac";
+	char b2[] = "A";
+	char b3[] = "";
+	char b4[] = "achg";
+	printf("%s\n", MyStrCpy(b1, b2));
+	printf("%s\n", MyStrCpy(b1, b3));
+	printf("%s\n\n", MyStrCpy(b1, b4));
 	
+	/*test strncmp*/
+	char *c1 = "comp";
+	char *c2 = "CO!MP";
+	char *c3 = "b";
+	char *c4 = "";
+	
+	printf("%s\n", strncpy(c1, c2, 2));
+	printf("%s\n", strncpy(c1, c3, 0));
+	printf("%s\n\n", strncpy(c1, c4, 6));
+
+	
+	printf("%s\n", mystrncpy(c1, c2, 2));
+	printf("%s\n", mystrncpy(c1, c3, 0));
+	printf("%s\n\n", mystrncpy(c1, c4, 6));
+
+    /*test strchr*/
+    char *a1 = "comp";
+	printf("%s\n", strchr(s1, 'o'));
+	printf("%s\n", strchr(s1, 'p'));
+	printf("%s\n\n", strchr(s1, 'a'));
+	
+	printf("%s\n", mystrchr(s1, 'o'));
+	printf("%s\n", mystrchr(s1, 'p'));
+	printf("%s\n\n", mystrchr(s1, 'a'));
 	
 	
 	return 0;
--- c/ws2/mystring.h
+++ c/ws2/mystring.h
@@ -16,7 +16,7 @@ size_t mystrLen(const char *s)
 	assert(NULL != s);
 	const char *runner = s;
 
-	while('\0' != *runner)
+	while ('\0' != *runner)
 	{
 		++runner;
 	}
@@ -32,7 +32,7 @@ int MyStrCmp(const char *s1, const char *s2)
 	const char *runner1 = s1, *runner2 = s2;
 	int i = 0;
 
-	while(*runner1 == *runner2 && '\0' != *runner1)
+	while (*runner1 == *runner2 && '\0' != *runner1)
 		{
 	    ++runner1;
 	    ++runner2;
@@ -48,7 +48,7 @@ char *MyStrCpy(char *dest, const char *src)
 	
 	char *destination = dest;
 
-	while('\0' != *src)
+	while ('\0' != *src)
 	{
 		*dest = *src;
 		++src;
@@ -68,7 +68,7 @@ char *mystrncpy(char *dest, const char *src, size_t n)
 	assert(NULL != dest && NULL != src);
 	
 	char *runner = dest;
-    int i = 0;
+    	int i = 0;
 	for(i = 0; i < n; i++)
 	{
 	*runner = src[i];
@@ -78,3 +78,37 @@ char *mystrncpy(char *dest, const char *src, size_t n)
 		
 	return dest;
 }
+
+/*********strcasecmp*******/
+
+int mystrcasecmp(const char *s1, const char *s2)
+{
+	assert(NULL != s1 && NULL != s2);
+	
+	const char *runner1 = s1, *runner2 = s2; 
+
+	while (tolower(*runner1) == tolower(*runner2) && '\0' != *runner1)
+		{
+	    ++runner1;
+	    ++runner2;
+		}
+	return (*runner1-*runner2);
+}
+
+
+/********strchr**********/
+
+char *strchr(const char *s, int c)
+{
+	assert(NULL != s);
+	
+	const char *runner = s;
+	
+	while (*runner!= c)
+	{
+		++runner;
+	}
+	return runner;
+}
+
+
