9e92dbecdd8c8373eb508dcc24c5361ee48dc754
shye.shapira <shyesh@gmail.com>
2020-03-22 10:34:19 +0200

    singleton test

--- fs/projects/src/il/co/ilrd/singleton/Singleton.java
+++ fs/projects/src/il/co/ilrd/singleton/Singleton.java
@@ -4,11 +4,11 @@ public class Singleton {
 	private static Singleton singleton;
 	private volatile static Singleton volatileSingleton; //volatile instance
     private static final Singleton notLazySingleton = new Singleton(); //the variable will be created when the class is loaded
-    
+       
     private Singleton (){}
     
     //A thread-safe singleton pattern in java using Synchronization
-	public synchronized static Singleton ThreadSafegetInstance() {
+	public synchronized static Singleton ThreadSafeSynchronization() {
 	   if(singleton == null) {
 	      singleton = new Singleton();
 	   }
@@ -39,21 +39,22 @@ public class Singleton {
         return volatileSingleton;
     }
 
-    //Thread-safe but not lay initialized 
+    //Thread-safe but not lazy initialized 
     //static final variable
     public static Singleton getNotLazySingleton(){
             return notLazySingleton;
     }
-    
-    //The ultimate Thread-safe and efficient singleton pattern in Java
+   
+/*    //The ultimate Thread-safe and efficient singleton pattern in Java
     // Correct lazy initialization in Java 
-    private static class SingletonHolder {
-        public static Singleton bestSingleton = new Singleton();
-     }
+    private static class LazyHolder {
+        static final Singleton INSTANCE = new Singleton();
+    }    
     
     public static Singleton getInstance() {
-        return SingletonHolder.bestSingleton;
-    }
+        return LazyHolder.INSTANCE;
+    }*/
+
     
     //Using Enum
     //The best way to implement a thread safe Singleton Pattern in Java is using Enum.
@@ -72,17 +73,50 @@ public class Singleton {
         public String toString() {
             return String.valueOf (this.nCode);
         }
-     
-    }
-     
-    public class ColorTest {
-        public void main(String[] args) {
-            Color red = Color.RED;
-            Color red2 = Color.RED;
-     
-            System.out.println(red == red2); // return true
-     
-        }
-    }
+    
+    
+}
+    
+    public static class Main{
+    	public static void main(String[] vars) {
+    		Singleton s1 = ThreadSafeSynchronization();
+    		for (int i = 0; i < 100; ++i) {
+    			System.out.println(s1 == ThreadSafeSynchronization());
+    		}
+    		
+    		System.out.println();
+    		
+    		Singleton s2 = DoubleCheckedgetInstance();
+    		for (int i = 0; i < 100; ++i) {
+    			System.out.println(s2 == DoubleCheckedgetInstance());
+    		}
+    		
+    		System.out.println();
+    		
+    		Singleton s3 = volatileSingleton();
+    		for (int i = 0; i < 100; ++i) {
+    			System.out.println(s3 == volatileSingleton());
+    		}
 
+    		System.out.println();
+    		
+    		Singleton s4 = getNotLazySingleton();
+    		for (int i = 0; i < 100; ++i) {
+    			System.out.println(s4 == getNotLazySingleton());
+    		}
+    		
+    		System.out.println();
+    		
+   
+    	   Color red = Color.RED;
+    	            
+    	   for (int i = 0; i < 100; ++i) {
+    	       	System.out.println(red == Color.RED); // return true
+    	   }
+    	     
+    	            
+    	    
+
+    	}
+    }
 }
---
+++ fs/projects/src/il/co/ilrd/singleton/SingletonTest.java
@@ -0,0 +1,15 @@
+package il.co.ilrd.singleton;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import il.co.ilrd.singleton.Singleton.Color;
+
+class SingletonTest {
+	
+	@Test
+	void test() {
+		
+	}
+}
